6dba0d07158df55909bb2afdc1cde4fff086b957
Bob McHenry <bobmchenryvideo@gmail.com>
2016-02-11 16:37:45 -0800

    Initial Commit

---
+++ ad325/data_structures/BooleanConstructorImplemented.java
@@ -0,0 +1,7 @@
+package ad325.data_structures;
+
+/**
+ * This is a "marker" interface. It simple marks that the
+ * boolean-parameter constructor is implemented.
+ */
+public interface BooleanConstructorImplemented {}
---
+++ ad325/test/data_structures/PQUnitTest.java
@@ -0,0 +1,11 @@
+package ad325.test.data_structures;
+
+import org.junit.*;
+import org.junit.Assert.*;
+
+public class PQUnitTest{
+	
+	PriorityQueueInterface<String> spq;
+	
+
+}
